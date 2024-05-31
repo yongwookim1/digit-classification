0684038dcd112205a20f703c544d995e7ae50fd8
junyoung1225 <jamiekang1050@gmail.com>
2024-05-31 23:42:48 +0900

    feat: add train loop

--- train.py
+++ train.py
@@ -18,3 +18,32 @@ def train(train_loader, valid_loader, args):
     optimizer = optim.Adam(model.parameters(), lr = args.lr, betas = (0.9, 0.999), eps = 1e-08)
     criterion = nn.CrossEntropyLoss()
     criterion.to(device)
+
+    bset_acc = 0.0
+    best_epoch = 1
+    for epoch in range(args.epochs):
+        print("-"*40)
+        print(f"Epoch : {epoch + 1}/{args.epochs}")
+        epoch_loss = 0.0
+        epoch_corrects = 0
+        model.train()
+        for batch_in, batch_out in tqdm(train_loader):
+            batch_in = batch_in.to(device)
+            batch_out = batch_out.to(device)
+
+            y_pred = model(batch_in)
+            preds = torch.argmax(y_pred,1)
+
+            loss = criterion(y_pred, batch_out)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            epoch_loss = epoch_loss + loss.item() * batch_in.size(0)
+            epoch_corrects = epoch_corrects + torch.sum(preds == batch_out.data)
+        
+        epoch_loss = epoch_loss / len(train_loader.dataset)
+        epoch_acc = epoch_corrects.double() / len(train_loader.dataset)
+
+        print(f"train_loss : {epoch_loss:.4f}" acc : {epoch_acc:.4f})
